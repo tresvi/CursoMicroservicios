e109387a2532fb1897ab925f6357f666d35f8810
tresvi <69699106+tresvi@users.noreply.github.com>
2023-06-12 01:40:58 -0300

    Agregado de instrucciones para levantar kafka local

--- Auditoria.Clientes.Consumer/Program.cs
+++ Auditoria.Clientes.Consumer/Program.cs
@@ -35,5 +35,21 @@ finally {
 }
 
 
+/*Hacerlo todo en consolas separadas y dejarlas corriendo
+ * 
+0)Instalacion: https://www.apache.org/dyn/closer.cgi?path=/kafka/2.1.0/kafka_2.11-2.1.0.tgz   (instalar el JDK de Java 8 tambien) 
+Descomprimir la carpeta en C:\tools 
+ 
+1)Levantar coordinador de transaccion "Zookeeper" (en puerto 2181 por defecto):
+PS C:\tools\kafka_2.11-2.1.0> .\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties 
 
+2) Levantar kafka (puerto 9092)
+PS C:\tools\kafka_2.11-2.1.0> .\bin\windows\kafka-server-start.bat .\config\server.properties
 
+3) Crear un topico
+PS C:\tools\kafka_2.11-2.1.0\bin\windows> .\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic auditoria-clientes
+
+4)(OPCIONAL) Verificar si se creó (se listan TODOS los topicos activos) 
+PS C:\tools\kafka_2.11-2.1.0\bin\windows> .\kafka-topics.bat --list --zookeeper localhost:2181
+
+*/
