1a64298e707ef55bdd2a2280e3261350bf12facd
tresvi <69699106+tresvi@users.noreply.github.com>
2023-06-24 01:13:14 -0300

    Se remueve metodos OnConfiguring de los context
    Se remueve el metodo OnCofiguring() de los context de ambos proyectos

--- Clientes.Web.Api/Models/ClientesContext.cs
+++ Clientes.Web.Api/Models/ClientesContext.cs
@@ -17,10 +17,6 @@ public partial class ClientesContext : DbContext
 
     public virtual DbSet<Cliente> Clientes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Clientes; Integrated Security = True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Cliente>(entity =>
